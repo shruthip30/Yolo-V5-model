6cbb023802425486606bd6ff66c556e7e5192e38
shruthip30 <108461398+shruthip30@users.noreply.github.com>
2022-07-07 03:13:13 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from logging import NullHandler
+import pandas as pd
+import numpy as np
+import streamlit as st
+from PIL import Image
+import torch
+import matplotlib.pyplot as plt
+
+st.title('Object Detection Model')
+
+uploaded_image = st.file_uploader('Upload the image you want to run through the ML Object Detection model', type = ['jpg','jpeg','png'],accept_multiple_files=True)
+
+cols = st.columns(2)
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path=r'C:\Users\Admin\Desktop\yolov5_results\Web App\yolov5\\runs\\train\\exp\\weights\\best.pt')
+
+count = 0
+for uploaded_file in uploaded_image:
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        # st.image(image, caption='Fashion Product Image',width=200)
+        result = model(image,size=640)
+        cols[count].header("YOLO trained image")
+        cols[count].subheader(count+1)
+        cols[count].image(np.squeeze(result.render()),width=300)
+        count = count + 1
+        
